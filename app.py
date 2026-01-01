f5f2e5fcfd931d8e2b6fb02af67aa1d1cb11df9c
Hanaakp <fathmahanakp@gmail.com>
2026-01-01 11:38:48 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain_classic.agents import initialize_agent,AgentType
+#arxiv-wikipedia inbuilt tools
+from langchain_community.tools import ArxivQueryRun,WikipediaQueryRun,DuckDuckGoSearchRun
+from langchain_community.utilities import WikipediaAPIWrapper,ArxivAPIWrapper
+from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
+import os
+from dotenv import load_dotenv
+load_dotenv()
+ #arxiv wikipedia tols
+
+api_wrapper_wiki=WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=250)
+wiki=WikipediaQueryRun(api_wrapper=api_wrapper_wiki)
+
+api_arxiv_wrapper=ArxivAPIWrapper(top_k_results=1,doc_content_chars_max=250)
+arxiv=ArxivQueryRun(api_wrapper=api_arxiv_wrapper)
+search = DuckDuckGoSearchRun(name="search")
+  #streamlit
+st.title("Search Engine with Tools and Agents")
+st.write("")
+#st sidebar
+st.sidebar.title("settings")
+api_key = st.sidebar.text_input("your groq api key:",type="password")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"]=[{"role":"assistant","content":"Hi,Iam A Serch engine chat bot, How can i help you?"}]
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg['content'])
+
+if prompt:=st.chat_input(placeholder="what is machine learning"):
+    st.session_state.messages.append({"role":"user","content":prompt})
+    st.chat_message("user").write(prompt)
+
+    llm=ChatGroq(groq_api_key=api_key,model_name="llama-3.3-70b-versatile",streaming=True)
+    tools=[search,wiki,arxiv]
+    search_agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handling_parsing_errors=True)
+
+    with st.chat_message("assistant"):
+        st_cb=StreamlitCallbackHandler(st.container(),expand_new_thoughts=False)
+        response=search_agent.run(st.session_state.messages,callbacks=st_cb['messages'])
+        st.session_state.messages.append({"role":"assistant","content":response})
